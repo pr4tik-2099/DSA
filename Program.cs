e126c15cdf9a7d85743b382975a1b38a672a4489
pr4tik-2099 <pratikmahato2099@gmail.com>
2026-02-25 23:22:30 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -3,7 +3,5 @@ using DSA.LINKED_LIST;
 using System;
 
 
-LinkedListTraverse linkedListTraverse = new LinkedListTraverse();
-//linkedListTraverse.Traverse();
 LinkedListInsertion LLI = new LinkedListInsertion();
 LLI.InsertAtBeginning();
